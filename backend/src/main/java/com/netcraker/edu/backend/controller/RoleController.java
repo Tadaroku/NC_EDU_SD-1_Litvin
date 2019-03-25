d193472e2acd89223ba89a349f50e009285a98e0
Edward <torresthebes@mail.ru>
2019-03-25 21:40:49 +0300

    add RoleController class

---
+++ backend/src/main/java/com/netcraker/edu/backend/controller/RoleController.java
@@ -0,0 +1,36 @@
+package com.netcraker.edu.backend.controller;
+
+import com.netcraker.edu.backend.entities.Role;
+import com.netcraker.edu.backend.service.interfaces.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/role")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Role> getAllRoles() {
+        return roleService.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Role saveRole(@RequestBody Role role) {
+        return roleService.save(role);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteRole(@PathVariable(name = "id") Long id) {
+        roleService.delete(id);
+    }
+
+}
