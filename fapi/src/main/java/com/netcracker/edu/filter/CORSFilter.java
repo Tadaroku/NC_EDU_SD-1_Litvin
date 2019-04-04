1277e4d8631d789c807e53479b00282ab1cb65c6
Edward <torresthebes@mail.ru>
2019-04-04 20:42:00 +0300

    add CORSFilter

---
+++ fapi/src/main/java/com/netcracker/edu/filter/CORSFilter.java
@@ -0,0 +1,31 @@
+package com.netcracker.edu.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
